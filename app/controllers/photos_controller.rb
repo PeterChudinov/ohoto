19fe44febfe687026c3f101dccdafc585fe559ed
PeterChudinov <haulhd@gmail.com>
2015-07-11 22:13:26 +0300

    -photos_controller

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,20 +1,5 @@
 class PhotosController < ApplicationController
   def index
     @recent_media = client.user_liked_media(access_token: current_user.access_token)
-    
-    image_ig_id = @recent_media[2].id
-    image_low_res_url = @recent_media[2].images.low_resolution.url
-    image_standard_res_url = @recent_media[2].images.standard_resolution.url
-    image_tags[] = @recent_media[2].tags
-    image_posted_at = @recent_media[2].created_at
-    image_link = @recent_media[2].link
-    image_likes_count = @recent_media[2].likes_count
-    image_author = @recent_media[2].user
-    image_caption = @recent_media[2].caption
-    image_curator = current_user.id
-   # respond_to do |format|
-   #   format.html
-   #   format.json { render :image_ig_id, :image_standard_res_url }
-   # end
   end
 end
