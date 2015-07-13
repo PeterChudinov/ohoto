b1a3eddbaf7da0b986c701ed98f946a5274797e2
PeterChudinov <haulhd@gmail.com>
2015-07-14 00:05:21 +0300

    to fix videos

--- db/schema.rb
+++ db/schema.rb
@@ -16,14 +16,6 @@ ActiveRecord::Schema.define(version: 20150712094814) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "Iikes", force: :cascade do |t|
-    t.string   "instagram_id"
-    t.string   "link"
-    t.integer  "user_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-  end
-
   create_table "items", force: :cascade do |t|
     t.string   "title"
     t.string   "image_url"
@@ -40,6 +32,14 @@ ActiveRecord::Schema.define(version: 20150712094814) do
   end
 
   add_index "items", ["shop_id"], name: "index_items_on_shop_id", using: :btree
+
+  create_table "likes", force: :cascade do |t|
+    t.string   "instagram_id"
+    t.string   "link"
+    t.integer  "user_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "sessions", force: :cascade do |t|
     t.string   "session_id", null: false
